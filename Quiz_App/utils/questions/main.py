7afa7330892c4f0f79741ba653b5a0bb702301a5
Manasseh <rougerapper@gmail.com>
2025-01-08 23:22:40 +0100

    I just created the files for each topic now to write the logic to get the questions

--- Quiz_App/utils/questions/main.py
+++ Quiz_App/utils/questions/main.py
@@ -6,16 +6,9 @@ Sort By Difficulty
 Sort By Question Type
 """
 
-fhand = open("questions_2.json",encoding='utf-8')
-
-content  = fhand.read().encode("utf-8",errors='replace') # type: ignore
-questions = json.loads(content)
-anime = open("anime.json","x")
-tech = open("tech.json","x")
-for question in questions:
-  if question["topic"] == "anime":
-    json.dump(question,anime)
-    del question
-  elif question["topic"] == "tech":
-    json.dump(question,tech)
-    del question
+fhand = open("questions_2.json","+")
+#gym and fitness
+#boxing
+#general knowledge
+#anime
+#tech
