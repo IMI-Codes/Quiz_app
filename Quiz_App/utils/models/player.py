4118837405f458dc1971087a4920f8ea11c3b27c
Manasseh <rougerapper@gmail.com>
2025-01-10 22:56:20 +0100

    I realize now that big project like these I have to have a plan I can't just free style like smaller scripts I have to do a bit of planning so starting tomorrow I will

---
+++ Quiz_App/utils/models/player.py
@@ -0,0 +1,25 @@
+class Player:
+  def __init__(self,name) -> None:
+    self.name = name
+    self.high_score = 0
+    self.best_question_type = None
+    self.fastest_completion_time = 0
+    self.best_topic = None
+    self.password = None#make everything a private attributes
+    
+    
+  
+  def create_player(self,name,pwd):
+    player_name = name
+  
+  
+  def check_player(self,name):
+    pass
+  
+  def validate_player_id(self):
+    pass # check if it's actual the player that is login in 
+  
+  
+  def display_stats(self):
+    pass
+
---
+++ Quiz_App/utils/models/quiz.py
@@ -0,0 +1,11 @@
+class Quiz:
+  def __init__(self) -> None:
+    pass
+  
+  def create_game(self):
+    pass
+  
+  
+class Easy_mode(Quiz):
+  def __init__(self) -> None:
+    super().__init__()
