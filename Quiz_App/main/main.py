6a6aa268bcf2496379ec4bdec44ae1ab08817eb3
Manasseh <rougerapper@gmail.com>
2025-01-15 23:17:34 +0100

    I'm expericening a block will try again tomorrow

--- Quiz_App/main/main.py
+++ Quiz_App/main/main.py
@@ -8,20 +8,26 @@ def main(win):
   player_statuses = ["New Player","Existing Player"]
   
   def print_options(area,options:list,message:Optional[str]=None):
-    
+    curses.init_pair(1,curses.COLOR_BLACK,curses.COLOR_MAGENTA)
+    current_row = 0
     if message:
       area.clear()
-      
-      for idx,value in enumerate(options):
-        area.addstr(idx+1,0,value)
+      area.addstr(0,0,message)
     else:
-      pass
+      pass #Come back to this use case
+      
     
+  print_options(win,player_statuses,"Please Pick One")
+  #win.getch()
+wrapper(main)
+""" 
+
+for idx,value in enumerate(options):
+        if current_row == idx:
+          area.addstr(idx+1,0,value,curses.color_pair(1))
+        else:
+          area.addstr(idx+1,0,value)
     while True:
       key = area.getch()
       #logic for selection
-    
-    
-  print_options(win,player_statuses,"What")
-  #win.getch()
-wrapper(main)
+ """    
